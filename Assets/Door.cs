8d8c2be6899cd2d53da8319529cc0bda950122fc
Adle0na <uuuuufgh@gmail.com>
2025-10-25 06:11:49 +0900

    <feat> 문생성 완료 확인

---
+++ Assets/Door.cs
@@ -0,0 +1,26 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [LabelText("문 이미지")]
+    public SpriteRenderer doorSprite;
+
+    [LabelText("문 데이터")]
+    public DoorData doorData;
+    public void ApplySpriteByType(Sprite setSprite)
+    {
+        if (doorSprite == null)
+        {
+            Debug.LogWarning($"{name}: doorSprite(SPR) 참조가 없습니다.");
+            return;
+        }
+        if (doorData == null)
+        {
+            Debug.LogWarning($"{name}: doorData가 null 입니다.");
+            return;
+        }
+
+        doorSprite.sprite = setSprite;
+    }
+}
--- Assets/Scripts/HSW/ApartmentFloor.cs
+++ Assets/Scripts/HSW/ApartmentFloor.cs
@@ -14,8 +14,8 @@ public class ApartmentFloor : MonoBehaviour
     [LabelText("칸 이미지 값")]
     [SerializeField] private List<SpriteRenderer> walls;
     
-    [LabelText("문 정보")] [SerializeField]
-    private List<DoorData> doorDatas;
+    [LabelText("문 정보")]
+    public List<Door> doorDatas;
     
     // 층 세팅 함수
     public void SetFloor(Sprite floorSprite)
@@ -23,12 +23,6 @@ public class ApartmentFloor : MonoBehaviour
         floorImage.sprite = floorSprite;
     }
 
-    private void SetDoor()
-    {
-        // 문생성
-        DoorData getDoor = new DoorData();
-    }
-    
     // 박스 생성처리
     private void SetBox()
     {
