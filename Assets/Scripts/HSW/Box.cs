f6b196943b048e18d42da8f28cb64ef0c7b24bbd
Adle0na <uuuuufgh@gmail.com>
2025-10-25 04:03:08 +0900

    <feat> 박스 리소스, enum값 수정

--- Assets/Scripts/HSW/Box.cs
+++ Assets/Scripts/HSW/Box.cs
@@ -1,7 +1,10 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class Box : MonoBehaviour
 {
+    [LabelText("박스 이미지")]
     public SpriteRenderer boxSprite;
+    [LabelText("박스 데이터")]
     public BoxData boxData;
 }
