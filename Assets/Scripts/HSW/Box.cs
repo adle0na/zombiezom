01460a8d9caf7f12fc4d1b5508d7554c0ed4e9d5
k56754665 <k56754665@gmail.com>
2025-10-25 04:36:01 +0900

    <feat> 두트윈 추가

--- Assets/Scripts/HSW/Box.cs
+++ Assets/Scripts/HSW/Box.cs
@@ -1,7 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
+using static IInteractable;
 
-public class Box : MonoBehaviour
+public class Box : MonoBehaviour, IInteractable
 {
     public SpriteRenderer boxSprite;
     public BoxData boxData;
+    public InteractHoldType HoldType { get; } = InteractHoldType.Long;
+    public bool IsInteractable => !boxData.isOpened;
+
+    private void Awake()
+    {
+        boxSprite = GetComponent<SpriteRenderer>();
+        boxData = new()
+        {
+            boxItems = new List<ItemData>()
+            {
+
+            },
+            boxType = BoxType.BloodBox_L,
+            isOpened = false,
+        };
+    }
+    
+    public void Interact()
+    {
+        Debug.Log("Interact Box");
+        boxData.isOpened = true;
+        // 암것도 없으면 플레이어 머리위에 팝업 띄우기
+        // 하나라도 있으면 아이템 생성해서 바닥에 뿌리기
+        // 열린 박스 스프라이트로 교체
+    }
+
+    public string GetInteractPrompt()
+    {
+        return "open";
+    }
 }
