eef2c687fbea304bbeffb50898b104b7b0d0bc21
gjkang <95170766+k56754665@users.noreply.github.com>
2025-12-02 02:32:17 +0900

    <fix> 메인화면 클릭 시 바로 StartScene으로 이동

--- Assets/SettingUI.cs
+++ Assets/SettingUI.cs
@@ -81,7 +81,7 @@ public class SettingUI : MonoBehaviour
         
         Time.timeScale = 1f;
         
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void CloseSettingPopup()
